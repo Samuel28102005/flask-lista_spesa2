254491851d3c1162a527a034c7ef4d83f315423c
Samuel28102005 <labarba.samuel@iisgalvanimi.edu.it>
2024-11-26 08:18:50 +0000

    lista 2 finito

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request, redirect, url_for
+#inizializza l'app Flask
+
+app = Flask(__name__)
+
+lista_spesa = ["pippo"]
+
+#rotta principale
+@app.route('/')
+def home():
+
+    return render_template('index.html',lista_spesa=lista_spesa)
+#
+
+@app.route('/aggiungi', methods=['POST'])
+def aggiungi():
+    elemento = request.form['elemento']
+    if elemento:
+        lista_spesa.append(elemento)
+    return redirect(url_for('home'))
+
+@app.route('/rimuovi/<int:indice>', methods=['POST'])
+def rimuovi(indice):
+    if 0 <= indice < len(lista_spesa):
+        lista_spesa.pop(indice)
+    return redirect(url_for('home'))
+
+@app.route('/svuota', methods = ['POST'])
+def svuota():
+    lista_spesa.clear()
+    return redirect(url_for('home'))
+
+#avvio dell'app Flask
+if __name__ == '__main__':
+    app.run(debug=True)
